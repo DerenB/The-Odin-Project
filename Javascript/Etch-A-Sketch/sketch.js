6ed79207d6d15d3d6d13a04b0a30ce1f91f2bfa1
Deren B <dbozerprogramming@gmail.com>
2022-02-14 22:23:58 -0500

    JS items

--- Javascript/Etch-A-Sketch/sketch.js
+++ Javascript/Etch-A-Sketch/sketch.js
@@ -1,7 +1,33 @@
+
+// Runs the function on page load
 document.body.onload = addElement;
 
+const mainDiv = document.getElementById("main");
+
+// Function to add the 16 x 16 grid
 function addElement() {
     for(let i = 0; i < 16; i++) {
-        
+        // Creates the Div row
+        let newDiv = document.createElement("div");
+
+        // Adds the class to the div row
+        newDiv.className = "etch-row";
+
+        // Appends the new row to Main
+        mainDiv.appendChild(newDiv);
+
+        for(let i = 0; i < 16; i++) {
+            let rowItem = document.createElement("div");
+            rowItem.className = "etch-item";
+            newDiv.appendChild(rowItem);
+        }
     }
+}
+
+// Function to reset grid items to white
+function reset() {
+	var elements = document.getElementsByClassName('etch-item');
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.backgroundColor = "white";
+	}
 }
