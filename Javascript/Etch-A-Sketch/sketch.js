7a0edb9aef154d6e9387bd0a1837abf9d901a2da
Deren B <dbozerprogramming@gmail.com>
2022-02-14 23:11:49 -0500

    Alert Prompts

--- Javascript/Etch-A-Sketch/sketch.js
+++ Javascript/Etch-A-Sketch/sketch.js
@@ -2,11 +2,14 @@
 // Runs the function on page load
 document.body.onload = addElement;
 
+
 const mainDiv = document.getElementById("main");
 
 // Function to add the 16 x 16 grid
-function addElement() {
-    for(let i = 0; i < 16; i++) {
+function addElement(row, column) {
+    removeAllChildNodes(mainDiv);
+
+    for(let i = 0; i < row; i++) {
         // Creates the Div row
         let newDiv = document.createElement("div");
 
@@ -16,7 +19,7 @@ function addElement() {
         // Appends the new row to Main
         mainDiv.appendChild(newDiv);
 
-        for(let i = 0; i < 16; i++) {
+        for(let i = 0; i < column; i++) {
             let rowItem = document.createElement("div");
             rowItem.className = "etch-item";
             newDiv.appendChild(rowItem);
@@ -32,10 +35,61 @@ function reset() {
 	}
 }
 
+function alertPrompt() {
+    let rowDimensions = prompt("Enter Row Value: ", "16");
+    let columnDimensions = prompt("Enter Column Value: ", "16");
+    let addRow;
+    let addColumn;
+    if(rowDimensions == null || rowDimensions == "") {
+        addRow = 1;
+    } else {
+        addRow = rowDimensions;
+    }
+    if(columnDimensions == null || columnDimensions == "") {
+        addColumn = 1;
+    } else {
+        addColumn = columnDimensions;
+    }
+    addElement(addRow,addColumn);
+}
 
-// var hoverItems = document.getElementsByClassName('etch-item');
-// for (i in hoverItems) {
-//     hoverItems[i].addEventListener("mouseover", function() {
-//         hoverItems[i].style.backgroundColor = "pink";
-//     });
-// }
+// Gets the enter box values
+function gridDimensions() {
+    // Logs the input values
+    console.log(document.getElementById("rows").value);
+    console.log(document.getElementById("columns").value);
+
+    // Determines the row value
+    let rows = document.getElementById("rows").value;
+    if (rows > 100) {
+        rows = 100;
+        alert("Maximum row size is 100.");
+    } else if (rows < 1) {
+        rows = 1;
+        alert("Minimum row size is 1.");
+    } else if (rows == null || rows == "") {
+        rows = 1;
+    }
+
+    // Determines the column value
+    let columns = document.getElementById("columns").value;
+    if (columns > 100) {
+        columns = 100;
+        alert("Maximum column size is 100.");
+    } else if (columns < 1) {
+        columns = 1;
+        alert("Minimum column size is 1.");
+    } else if (columns == null || rows == "") {
+        columns = 1;
+    }
+
+    
+    addElement(rows,columns);
+}
+
+// Removes child elements
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
